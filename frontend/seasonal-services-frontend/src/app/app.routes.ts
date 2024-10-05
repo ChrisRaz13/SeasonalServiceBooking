7438a414259506aaec031011016ac35e54509e35
Christopher Rasmussen <Chris.E.Rasmussen1@gmail.com>
2024-10-05 04:30:15 -0400

    fixed edit clients

--- frontend/seasonal-services-frontend/src/app/app.routes.ts
+++ frontend/seasonal-services-frontend/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { provideRouter, RouterModule, Routes } from '@angular/router';
 import { ClientListComponent } from './components/client-list/client-list.component';
 import { BookingFormComponent } from './components/booking-form/booking-form.component';
 import { HomeComponent } from './components/home/home.component';
+import { ClientFormComponent } from './components/client-form/client-form.component';
 
-export const routes: Routes = [
+export const routes = [
   { path: '', component: HomeComponent },
   { path: 'clients', component: ClientListComponent },
+  { path: 'clients/new', component: ClientFormComponent },
+  { path: 'clients/edit/:id', component: ClientFormComponent }, // Edit route with ':id' parameter
   { path: 'bookings', component: BookingFormComponent }
 ];
+
