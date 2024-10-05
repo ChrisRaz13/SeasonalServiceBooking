8a88b7cf8c4197c3f39c685e5551b8ced5bb2f31
Christopher Rasmussen <Chris.E.Rasmussen1@gmail.com>
2024-10-05 02:20:36 -0400

    frontend started

--- backend/SeasonalServices/src/main/java/com/seasonalservices/controller/ClientController.java
+++ backend/SeasonalServices/src/main/java/com/seasonalservices/controller/ClientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,34 +22,53 @@ import com.seasonalservices.service.ClientService;
 @RequestMapping("/api/clients")
 public class ClientController {
 
-    private final ClientService clientService;
-    
-    @Autowired
-    public ClientController(ClientService clientService) {
-        this.clientService = clientService;
-    }
-    
-    @GetMapping
-    public ResponseEntity<List<Client>> getAllClients() {
-        List<Client> clients = clientService.getAllClients();
-        return ResponseEntity.ok(clients);
-    }
-    @PostMapping
-    public ResponseEntity<Void> addClient(@RequestBody Client client) {
-        clientService.addClient(client);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateClient(@PathVariable int id, @RequestBody Client client) {
-        client.setId(id);
-        clientService.updateClient(client);
-        return ResponseEntity.ok().build();
-    }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteClient(@PathVariable int id) {
-        clientService.deleteClient(id);
-        return ResponseEntity.noContent().build();
-    }
+	private final ClientService clientService;
 
-    
+	@Autowired
+	public ClientController(ClientService clientService) {
+		this.clientService = clientService;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Client>> getAllClients() {
+		List<Client> clients = clientService.getAllClients();
+		return new ResponseEntity<>(clients, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> getClientById(@PathVariable int id) {
+		Client client = clientService.getClientById(id);
+		if (client != null) {
+			return new ResponseEntity<>(client, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<Client> addClient(@RequestBody @Validated Client client) {
+		Client savedClient = clientService.addClient(client);
+		return new ResponseEntity<>(savedClient, HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Client> updateClient(@PathVariable int id, @RequestBody @Validated Client client) {
+		client.setId(id);
+		Client updatedClient = clientService.updateClient(client);
+		if (updatedClient != null) {
+			return new ResponseEntity<>(updatedClient, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteClient(@PathVariable int id) {
+		boolean isDeleted = clientService.deleteClient(id);
+		if (isDeleted) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
