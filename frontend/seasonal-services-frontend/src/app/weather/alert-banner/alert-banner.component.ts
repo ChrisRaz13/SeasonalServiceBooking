6d589e0913417bda42ce62b7f0156179cf7428dc
Christopher Rasmussen <Chris.E.Rasmussen1@gmail.com>
2024-11-03 05:50:45 -0400

    made a weather folder with the weather dashboard components broken up to be easier to work with and readability

---
+++ frontend/seasonal-services-frontend/src/app/weather/alert-banner/alert-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert-banner.component.html',
+  styleUrl: './alert-banner.component.css'
+})
+export class AlertBannerComponent {
+
+}
---
+++ frontend/seasonal-services-frontend/src/app/weather/day-forecast/day-forecast.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-forecast',
+  standalone: true,
+  imports: [],
+  templateUrl: './day-forecast.component.html',
+  styleUrl: './day-forecast.component.css'
+})
+export class DayForecastComponent {
+
+}
